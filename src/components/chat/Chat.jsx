78897977666770236d8898283f4f81287e366bf7
DHRUVRAJSAXENA <dhruvrajsaxena1234@gmail.com>
2024-07-16 10:22:51 +0530

    second comit

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,120 @@
-import React from "react"
+import React, { useState } from "react"
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
+
 const Chat = () => {
-  return <div className="chat">Chat</div>
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji)
+    setOpen(false)
+  }
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              nostrum qui possimus laudantium soluta perferendis nisi voluptates
+              libero architecto iste!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              quisquam accusantium deleniti a eligendi doloremque soluta
+              architecto
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              nostrum qui possimus laudantium soluta perferendis nisi voluptates
+              libero architecto iste!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              quisquam accusantium deleniti a eligendi doloremque soluta
+              architecto
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              nostrum qui possimus laudantium soluta perferendis nisi voluptates
+              libero architecto iste!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="./bg3.jpeg" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              quisquam accusantium deleniti a eligendi doloremque soluta
+              architecto
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          value={text}
+          placeholder="Type a message..."
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={() => setOpen(!open)} />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  )
 }
 
 export default Chat
